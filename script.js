717f8c1eb358604c7016a59d7f6dd23ac9fec669
haidhal <haidhalbabu@gmail.com>
2025-01-21 16:23:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// function sendMail(){
+//     let params = {
+//         name:document.getElementById("name").value,
+//         email:document.getElementById("email").value,
+//         subject:document.getElementById("subject").value,
+//         message:document.getElementById("message").value
+//     }
+//     emailjs.send("service_2rxhq4s","template_ftyflse",params).then(alert("email sentt"))
+// }
